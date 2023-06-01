3fa1112d3182ff5bb248aed7fdd80dfd047c36bf
Nur Muhammad Alhakim <alhakim2318@gmail.com>
2023-06-01 10:36:34 +0700

    Testing

---
+++ capstone-cc/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import pickle
+from urllib.request import urlopen
+import utils
+
+app = Flask(__name__)
+# f = open('model.pickle', 'rb')
+# model = pickle.load(f)
+model = pickle.load(urlopen("https://storage.googleapis.com/trippal-ml-model/model.pickle"))
+
+# Still error
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        data = request.data['input']
+   
+        model_input = utils.preprocessing(data)
+        results = model.predict(model_input)
+    return jsonify({"prediction": results})
