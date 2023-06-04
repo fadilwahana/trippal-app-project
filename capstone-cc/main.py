fd0715b5ea4cac0a48a8db385d9ce3365a3ccae5
Nur Muhammad Alhakim <alhakim2318@gmail.com>
2023-06-04 18:42:13 +0700

    Update

--- capstone-cc/main.py
+++ capstone-cc/main.py
@@ -1,22 +1,23 @@
-from flask import Flask, request, jsonify
-import pickle
+from fastapi import FastAPI
+from pydantic import BaseModel
 from urllib.request import urlopen
-import utils
+import joblib
 
-app = Flask(__name__)
+app = FastAPI()
 # f = open('model.pickle', 'rb')
 # model = pickle.load(f)
-model = pickle.load(urlopen("https://storage.googleapis.com/trippal-ml-model/model.pickle"))
+model = joblib.load(open('model-2.h5', 'rb'))
 
-# Still error
-@app.route('/predict', methods=['POST'])
-def predict():
-    if request.method == 'POST':
-        data = request.data['input']
-   
-        model_input = utils.preprocessing(data)
-        results = model.predict(model_input)
-    return jsonify({"prediction": results})
+class User(BaseModel):
+    name: str
+    bag_of_words: str 
+
+@app.post('/predict', tags=['PREDICT'])
+def predict(user: User):
+    return {
+        'data': user
+    }
 
 if __name__ == '__main__':
     app.run(port=9000, debug=True)
+    print(model)
