a635e3e7dce4291edd69bd838a496b5c31f8d60d
Nur Muhammad Alhakim <alhakim2318@gmail.com>
2023-06-09 14:31:18 +0700

    Update API without ML yet

--- capstone-cc/main.py
+++ capstone-cc/main.py
@@ -1,23 +1,9 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
-from urllib.request import urlopen
-import joblib
-
-app = FastAPI()
-# f = open('model.pickle', 'rb')
-# model = pickle.load(f)
-model = joblib.load(open('model-2.h5', 'rb'))
-
-class User(BaseModel):
-    name: str
-    bag_of_words: str 
-
-@app.post('/predict', tags=['PREDICT'])
-def predict(user: User):
-    return {
-        'data': user
-    }
+import uvicorn
+    
 
 if __name__ == '__main__':
-    app.run(port=9000, debug=True)
-    print(model)
+    # Run apps on GCP
+    # uvicorn.run('app.app:app', host='0.0.0.0', port=8000, reload=True)
+
+    # Run apps locally
+    uvicorn.run('app.app:app', port=8000, reload=True)
